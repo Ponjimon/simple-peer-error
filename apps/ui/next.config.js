658413b9574c32c19fcef7b7a3b59a8a896a17f3
Simon <sn@hitbox.tv>
2020-11-04 01:07:20 +0100

    chore: remove unneeded vars

--- apps/ui/next.config.js
+++ apps/ui/next.config.js
@@ -17,9 +17,6 @@ const nextConfig = {
   },
   env: {
     SERVER_URL: process.env.SERVER_URL,
-    PEER_HOST: process.env.PEER_HOST,
-    PEER_PORT: process.env.PEER_PORT,
-    PEER_PATH: process.env.PEER_PATH,
   },
 };
 
